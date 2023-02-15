a16d25c5c8c5ffa8fc012534a64a10b0b838dbd6
Xavier <xmedinavei@gmail.com>
2023-02-15 11:31:01 -0500

    Booking Viewsets

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,9 +1,17 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-from .views import MenuItemsView, SingleMenuItemView
+from .views import MenuItemsView, SingleMenuItemView, BookingViewSet
 
+
+router = DefaultRouter()
+router.register(r'tables', BookingViewSet, basename='tables')
 
 urlpatterns = [
     path('menu/', MenuItemsView.as_view()),
     path('menu/<int:pk>', SingleMenuItemView.as_view()),
+
+    path('booking/', include(router.urls)),
 ]
+
+# urlpatterns += router.urls
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,22 @@
+from django.contrib.auth.models import User
+from rest_framework import viewsets
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
 
-from .models import Menu
-from .serializers import MenuModelSerializer
+from .models import Menu, Booking
+from .serializers import MenuModelSerializer, BookingModelSerializer, UserModelSerializer
+
+
+# User
+class UserViewSet(viewsets.ModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UserModelSerializer
+   # permission_classes = [permissions.IsAuthenticated]
+
+# Booking
+class BookingViewSet (viewsets.ModelViewSet):
+    serializer_class = BookingModelSerializer
+    queryset = Booking.objects.all()
+
 
 # Menu
 class MenuItemsView(ListCreateAPIView):
